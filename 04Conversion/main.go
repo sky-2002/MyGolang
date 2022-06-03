a4cd5c2ca97f030dee67f7893731d2a896ab9240
Aakash Thatte <aathatte2002@gmail.com>
2022-06-03 10:08:18 +0530

    Explore user input, strings, and conversions

---
+++ 04Conversion/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Welcome to pizza app")
+	fmt.Println("Please rate our pizza between 1 and 5")
+
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+
+	fmt.Println("Thanks for rating", input)
+
+	numRating, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+
+	if err != nil {
+		fmt.Println("The err is", err)
+		fmt.Println("NumRating is", numRating)
+	} else {
+		fmt.Println("Added 1 to rating:", numRating+1)
+	}
+
+}
