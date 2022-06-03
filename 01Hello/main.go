962e750f7550a4e3e1961ab776bff757c1bae081
Aakash Thatte <aathatte2002@gmail.com>
2022-06-03 20:02:54 +0530

    Add code shortcuts

--- 01Hello/main.go
+++ 01Hello/main.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	// just type pkgm to get package main and main function setup
+	// just type fp or ff to get fmt.Println or fmt.Printf
 	fmt.Println("Hello from Aakash")
 }
