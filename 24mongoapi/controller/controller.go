8895d1c02ffc3539ec94a064ae3f052fb8861e98
Aakash Thatte <aathatte2002@gmail.com>
2022-06-06 14:31:15 +0530

    Add controller to get all movies

--- 24mongoapi/controller/controller.go
+++ 24mongoapi/controller/controller.go
@@ -2,8 +2,10 @@ package controller
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/sky-2002/mongoapi/model"
 	"go.mongodb.org/mongo-driver/bson"
@@ -110,4 +112,12 @@ func getAllMovies() []primitive.M {
 		movies = append(movies, movie)
 	}
 	return movies
+}
+
+// controllers
+func GetAllMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form--urlencode")
+	allMovies := getAllMovies()
+
+	json.NewEncoder(w).Encode(allMovies)
 }
