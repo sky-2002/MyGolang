4c086f643afb5b9157f2ab573fd6cf5e1ecbbf48
Aakash Thatte <aathatte2002@gmail.com>
2022-06-27 22:12:13 +0530

    Check waitgroup

--- Concurrency/waitgroup/main.go
+++ Concurrency/waitgroup/main.go
@@ -1,24 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"sync"
 )
 
 func main() {
+	// var beeper sync.WaitGroup
+
+	// ninjas := []string{"Tommy", "John", "Adam", "James"}
+	// beeper.Add(len(ninjas))
+
+	// for _, n := range ninjas {
+	// 	go attack(n, &beeper)
+	// }
+	// beeper.Wait()
+	// fmt.Println("Mission completed")
+
 	var beeper sync.WaitGroup
-
-	ninjas := []string{"Tommy", "John", "Adam", "James"}
-	beeper.Add(len(ninjas))
-
-	for _, n := range ninjas {
-		go attack(n, &beeper)
-	}
+	beeper.Add(1)
+	beeper.Done() // without this, it will result in a deadlock
+	// beeper.Done() // adding this twice gives a negative waitgroup counter, then add 2 and it will be ok
 	beeper.Wait()
-	fmt.Println("Mission completed")
+
 }
 
-func attack(ninja string, beeper *sync.WaitGroup) {
-	fmt.Println("Attacked", ninja)
-	beeper.Done()
-}
+// func attack(ninja string, beeper *sync.WaitGroup) {
+// 	fmt.Println("Attacked", ninja)
+// 	beeper.Done()
+// }
